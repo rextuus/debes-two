e43186fcf71e19c0e3935dccba1e994b0f54c7e0
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-11-16 13:24:08 +0100

    wip

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,7 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
